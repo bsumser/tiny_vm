8c7f7279af47e2b406a8d2e90295e141a929dc62
Brett Sumser <bsumser@gmail.com>
2024-04-29 14:17:00 -0700

    changing rules

--- grammar.py
+++ grammar.py
@@ -14,22 +14,32 @@ quack = Lark (r"""
     
     class : ident ";"
 
-    statement : assignment
-              | r_exp
+    statement : assignment ";"
+              | r_exp ";"
+              | l_exp ";"
+              | return_statement ";"
 
-    assignment : ident "=" int ";"
-              | ident "=" r_exp ";"
+    assignment : l_exp ":" ident "=" r_exp
+
+    l_exp : ident
+            | r_exp "." ident ";"
+
     r_exp : int
-              | r_exp "+" r_exp ";"
-              | r_exp "-" r_exp ";"
-              | r_exp "*" r_exp ";"
-              | r_exp "/" r_exp ";"
-              | "-" r_exp ";"
+              | r_exp "+" r_exp
+              | r_exp "-" r_exp
+              | r_exp "*" r_exp
+              | r_exp "/" r_exp
+              | "-" r_exp
+
+    return_statement: "return" r_exp
+                        | "return" l_exp
     %ignore WS
 
 """, start = "program")
 
 directory = "./test_progs"
+success = '\x1b[6;30;42m' + 'Success!' + '\x1b[0m'
+fail = '\x1b[0;30;41m' + 'FAIL!' + '\x1b[0m'
 
 for filename in os.listdir(directory):
     file = os.path.join(directory, filename)
@@ -37,8 +47,9 @@ for filename in os.listdir(directory):
         text = open(file).read()
         try:
             res = quack.parse(text).pretty()
-            print(res)
-            print(f"Prog {file} passed")
-        except:
-            print("Exception occured for file %s" % file)
+            #print(res)
+            print(f"Prog {file} passed......" + success)
+        except Exception as e:
+            print(("Exception for file %s......" + fail) % file)
+            print(e)
             continue
