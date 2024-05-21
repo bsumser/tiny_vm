153381a361d5b64d6cf3119fad7d7d6776425a6c
Brett Sumser <bsumser@gmail.com>
2024-05-21 11:32:15 -0700

    tree outputing

--- AST.py
+++ AST.py
@@ -1,40 +1,111 @@
 import logging
+from graphviz import Digraph
+
 logging.basicConfig()
 log = logging.getLogger(__name__)
 log.setLevel(logging.DEBUG)
 
 class ASTNode:
-    def __init__(self, data):
+    def __init__(self):
+        self.children = []
         print(f"init ASTNode of class {self.__class__.__name__}")
-        self.children = data
-        print(f"{self.children}")
-    
+
     def walk(self):
         for child in self.children:
-            log.debug(f"visit ASTNode of class {child.__class__.__name__}")
-            try:
-                child.walk()
-            except Exception as e:
-                log.error(f"fail walk {self.__class__.__name__} to {child.__class__.__name__}")
+            child.walk()
+    def __repr__(self):
+        return repr(self.__dict__)
 
-    def plus(self, e):
-        log.debug("-> plus")
-        left, right = e
-        return left + right
-    
-    def r_eval(self, buffer: list[str]):
-        print("not implemented")
+    def to_graphviz(self, graph=None, parent_id=None):
+        if graph is None:
+            graph = Digraph()
+
+        node_id = str(id(self))
+        label = self.__class__.__name__
+        if isinstance(self, OpHelp):
+            label += f' ({self.op})'
+        elif isinstance(self, NumberNode):
+            label += f' ({self.value})'
+        elif isinstance(self, IdentNode):
+            label += f' ({self.name})'
+
+        graph.node(node_id, label)
+
+        if parent_id is not None:
+            graph.edge(parent_id, node_id)
+
+        for child in self.children:
+            print(type(child))
+            child.to_graphviz(graph, node_id)
+
+        return graph
+
+                
 
 class ProgramNode(ASTNode):
-    pass
+    def __init__(self, statements):
+        self.statements = statements
+        self.children = statements
 
+class AssigNode(ASTNode):
+    def __init__(self, var_type, var_name, value):
+        self.var_type = var_type
+        self.var_name = var_name
+        self.value = value
+        self.children = [var_type, var_name, value]
 
-class AddNode(ASTNode):
-    def __init__(self, data):
-        print(f"init ASTNode of class {self.__class__.__name__}")
-        self.children = data
-        print(f"{self.children}")
-        self.buffer = []
+class OpHelp(ASTNode):
+    def __init__(self, left, right, op):
+        self.left = left
+        self.right = right
+        self.op = op
+        self.children = [left, right]
+    
+    def walk(self):
+        print(self.children)
 
-class Number(ASTNode):
-    pass
+class NumberNode(ASTNode):
+    def __init__(self, value):
+        self.value = value
+        self.children = value
+    def walk(self):
+        print(self.value)
+    
+    def to_graphviz(self, graph=None, parent_id=None):
+        if graph is None:
+            graph = Digraph()
+
+        node_id = str(id(self))
+        label = self.__class__.__name__
+        graph.node(node_id, label)
+
+        if parent_id is not None:
+            graph.edge(parent_id, node_id)
+
+        return graph
+
+class IdentNode(ASTNode):
+    def __init__(self, name):
+        self.name = name
+        self.children = name
+    
+    def walk(self):
+        print(self.name)
+    
+    def to_graphviz(self, graph=None, parent_id=None):
+        if graph is None:
+            graph = Digraph()
+
+        node_id = str(id(self))
+        label = self.__class__.__name__
+        graph.node(node_id, label)
+
+        if parent_id is not None:
+            graph.edge(parent_id, node_id)
+
+        return graph
+
+class R_ExpNode(ASTNode):
+    def __init__(self, expression):
+        self.expression = expression
+        self.children = [expression]
--- grammar.py
+++ grammar.py
@@ -1,36 +1,44 @@
-import lark
-import os
+from lark import Lark, Transformer, v_args
 from AST import *
 
-class QuackTransformer(lark.Transformer):
-    def program(self, data):
-        programNode = ProgramNode(data)
-        print(programNode.children)
-        return programNode
-    def r_exp(self, data):
-        print(f"r_exp node {data}")
-    def sum(self, data):
-        print(f"add node {data}")
-        buffer = []
-        buffer.append(data[0])
-        buffer.append(data[1])
-        buffer.append("call Int:plus")
-        addNode = AddNode(data)
-        addNode.buffer = buffer
-        return addNode
-    def number(self, data):
-        #"Convert the value of `tok` from string to int, while maintaining line number & column."
-        print(f"number node {data}")
-        return "const " + " " + data[0]
+
+@v_args(inline=True)
+class QuackTransformer(Transformer):
+    def program(self, *statements):
+        return ProgramNode(statements)
+
+    def assignment(self, var_type, var_name, value):
+        return AssigNode(var_type, var_name, value)
+    
+    def add(self, left, right):
+        return OpHelp(left, right, '+')
+
+    def sub(self, left, right):
+        return OpHelp(left, right, '-')
+
+    def mul(self, left, right):
+        return OpHelp(left, right, '*')
+
+    def div(self, left, right):
+        return OpHelp(left, right, '/')
+
+    def number(self, value):
+        return NumberNode(int(value))
+
+    def ident(self, name):
+        return IdentNode(name)
+
+    def neg(self, value):
+        return OpHelp(NumberNode(0), value, '-')  # Unary minus can be treated as 0 - value
+    
+    def r_exp(self, expression):
+        return R_ExpNode(expression)
 
 
-directory = "./test_progs"
-success = '\x1b[6;30;42m' + 'Success!' + '\x1b[0m'
-fail = '\x1b[0;30;41m' + 'FAIL!' + '\x1b[0m'
 
 def main():
     gram_file = open("grammar.lark", "r")
-    parser = lark.Lark(gram_file)
+    parser = Lark(gram_file)
     src_file = open("./test_progs/r_exp_test.qk", "r")
     src_text = "".join(src_file.readlines())
     parse_tree = parser.parse(src_text)
@@ -39,32 +47,10 @@ def main():
 
     transformer = QuackTransformer()
     ast = transformer.transform(parse_tree)
-    print(ast)
-    print(f"as {repr(ast)}")
-
-def prog_checker():
-    for filename in os.listdir(directory):
-        file = os.path.join(directory, filename)
-        test_summary = []
-        if os.path.isfile(file):
-            text = open(file).read()
-            try:
-                tree = quack.parse(text)
-                res_print = tree.pretty()
-                print(res_print)
-                parse = f"Prog {file} parse......" + success 
-                print(parse)
-                transform = f"Prog {file} transform......" + success 
-                print(transform)
-                ans = QuackTransformer().transform(tree)
-                print(f"Quack Transform return answer is {ans}")
-            except Exception as e:
-                exception = ("Exception for file %s......" + fail) % file
-                print(exception)
-                print(e)
-                test_summary.append(exception)
-                continue
-
+    print(f"ast is {repr(ast)}")
+    print(repr(ast.walk()))
+    graph = ast.to_graphviz()
+    graph.render('ast', format='png', view=True)
 
 if __name__=="__main__": 
     main()
