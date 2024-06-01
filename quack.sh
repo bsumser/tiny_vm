2f00a9ca1843f2c0ffecc8c8d76ce2dbc3258efd
Brett Sumser <bsumser@gmail.com>
2024-06-01 12:29:53 -0700

    working compilation script

--- quack.sh
+++ quack.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
+TVMDIR=~/tiny_vm
 if [ -f "$1" ]; then
     echo "Compiling $1"
-    python3 compiler.py $1 
+    python3 compiler.py $1
+    echo $?
+    if [ $? -eq 0 ]; 
+    then
+        echo "success"
+        python3 assemble.py QkAsm/qkmain.asm > ${TVMDIR}/OBJ/\$Main.json
+        if [ $? -eq 0 ]; then
+            bin/tiny_vm \$Main
+        fi
+    fi
 fi
